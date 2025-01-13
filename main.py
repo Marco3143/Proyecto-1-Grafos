23c0e84d2217e0cc156b485ae7787c1f86914f1c
Marco3143 <mlopezo2024@cic.ipn.mx>
2025-01-13 16:26:39 -0600

    Revert "Proyecto 6 segunda parte"

---
+++ main.py
@@ -0,0 +1,21 @@
+import Grafos
+import networkx as nx
+
+#s = Pruebas.Grafo_Erdos_Renyi(5,.44)
+#nx.write_graphml(s.Crea_gra_arch(), "grafo_EyR..graphml")
+#s=Pruebas.Grafo_Gilbert(20,190)
+#nx.write_graphml(s.Crea_gra_arch(), "grafo_Gilbert.graphml")
+#s = Pruebas.Barabasi_Albert(10,4)
+#nx.write_graphml(s.Crea_gra_arch(), "grafo_Barabasi_Albert.graphml")
+#s = Pruebas.Dorogovtsev_Mendes(10)
+#Pruebas.Imp_Grafo(s)
+#nx.write_graphml(s.Crea_gra_arch(), "grafo_Dorogovtsev_Mendes.graphml")
+#s = Pruebas.geografico_simple(10,0.33)
+#nx.write_graphml(s.Crea_gra_arch(), "grafo_geografico.graphml")
+#s = Grafos.graf_Malla(3,3)
+#Grafos.Imp_Grafo(s)
+#nx.write_graphml(s.Crea_gra_arch(), "grafo_malla.graphml")
+
+
+
+
